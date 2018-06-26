84ae30f47c08cb1a088a97ac2e63ee0f9bb01f8d
chenyinkai <287696322@qq.com>
2018-06-26 18:29:39 +0800

    增加关于测试

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import { Icon } from 'antd'
+import Avatars from '../../assets/avatars.jpeg'
 
 class About extends React.Component {
   render() {
     return (
       <main>
         <div>
-          <img src="../../assets/avatars.jpeg" alt="" />
+          <img src={Avatars} alt="" />
         </div>
         <div>善良的乌贼</div>
         <div className="subtitle">请不要假装很努力,因为结果不会陪你演戏。</div>
-        <Icon type="github" style={{ fontSize: '20px' }} />
+        <div>社交与联系</div>
+        <a href="https://github.com/chenyinkai">
+          <Icon type="github" style={{ fontSize: '16px' }} />
+          <span>Github</span>
+        </a>
+        <a href="https://github.com/chenyinkai">
+          <Icon type="rocket" style={{ fontSize: '16px' }} />
+          <span>掘金</span>
+        </a>
+        <a href="https://github.com/chenyinkai">
+          <Icon type="star" style={{ fontSize: '16px' }} />
+          <span>segmentfault</span>
+        </a>
+        <a href="https://github.com/chenyinkai">
+          <Icon type="mail" style={{ fontSize: '16px' }} />
+          <span>chenyinkai0223@gmail.com</span>
+        </a>
+        <a href="https://github.com/chenyinkai">
+          <Icon type="qq" style={{ fontSize: '16px' }} />
+          <span>287696322</span>
+        </a>
       </main>
     )
   }
